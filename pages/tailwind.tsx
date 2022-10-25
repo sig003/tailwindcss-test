85c2b036c6a2bfdbad23a222dff5be6104cbb4fd
sig03 <sig03@naver.com>
2022-10-25 00:25:03 +0000

    changes class naming

--- pages/tailwind.tsx
+++ pages/tailwind.tsx
@@ -4,36 +4,36 @@ export default function Tailwind() {
   const code = 'bg-gray-100 rounded-[5px] px-3 text-[1.1rem] w-[188px] h-[45px]';
   return (
     <>
-      <div class="h-screen flex flex-col justify-center items-center sm:h-fit mt-12">
-        <div class="h-[6rem] flex items-center">
-          <h1 class="text-[4rem]">Welcome to <span class="text-blue-500">Next.js!</span></h1>
+      <div className="h-screen flex flex-col justify-center items-center sm:h-fit mt-12">
+        <div className="h-[7rem] flex items-center">
+          <h1 className="text-[4rem] mt-[5rem]">Welcome to <span className="text-blue-500">Next.js!</span></h1>
         </div>
-        <div class="h-[36px] text-2xl my-16">Get started by editing <code class={code}>pages/index.tsx</code></div>
-        <div class="flex flex-row sm:flex-col">
+        <div className="h-[40px] text-2xl my-16">Get started by editing <code className={code}>pages/index.tsx</code></div>
+        <div className="flex flex-row sm:flex-col">
           <div>
-            <div class={contentsBorder}>
-              <span class="text-2xl">Documentation →</span>
-              <p class={message}>Find in-depth information about Next.js features and API.</p>
+            <div className={contentsBorder}>
+              <span className="text-2xl">Documentation →</span>
+              <p className={message}>Find in-depth information about Next.js features and API.</p>
             </div>
-            <div class={contentsBorder}>
-              <span class="text-2xl">Learn →</span>
-              <p class={message}>Learn about Next.js in an interactive course with quizzes!</p>
+            <div className={contentsBorder}>
+              <span className="text-2xl">Learn →</span>
+              <p className={message}>Learn about Next.js in an interactive course with quizzes!</p>
             </div>
           </div>
           <div>
-            <div class={contentsBorder}>
-              <span class="text-2xl">Examples →</span>
-              <p class={message}>Discover and deploy boilerplate example Next.js projects.</p>
+            <div className={contentsBorder}>
+              <span className="text-2xl">Examples →</span>
+              <p className={message}>Discover and deploy boilerplate example Next.js projects.</p>
             </div>
-            <div class={contentsBorder}>
-              <span class="text-2xl">Deploy →</span>
-              <p class={message}>Instantly deploy your Next.js site to a public URL with Vercel.</p>
+            <div className={contentsBorder}>
+              <span className="text-2xl">Deploy →</span>
+              <p className={message}>Instantly deploy your Next.js site to a public URL with Vercel.</p>
             </div>
           </div>
         </div>
-        <div class="w-[95%] border-solid border-t-2 border-gray-100 mt-28">
+        <div className="w-[95%] border-solid border-t-2 border-gray-100 mt-28">
           Powered by
-          <img src="/vercel.svg" />
+          <img src="/vercel.svg" width="72px" height="16px" />
         </div>
       </div>
       
